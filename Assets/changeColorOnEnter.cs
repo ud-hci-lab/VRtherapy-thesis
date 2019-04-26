85cf41b88596846422a14db9ecbdfb697e36c2b8
Brian Cohn <brian.cohn@usc.edu>
2019-04-26 11:11:10 -0700

    initialization without R hand model

---
+++ Assets/changeColorOnEnter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class changeColorOnEnter : MonoBehaviour
+{
+    private Color pre = Color.red;
+    private Color post = Color.green;
+    Material m_Material;
+
+    private bool hasBeenGreened = false;
+    void Start()
+    {
+        m_Material = GetComponent<Renderer>().material;
+        m_Material.color = pre;
+        hasBeenGreened = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            m_Material.color = pre;
+            hasBeenGreened = false;
+
+        }
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        m_Material.color = post;
+        if (hasBeenGreened == false)
+        {
+            hasBeenGreened = true;
+            AudioSource audd = GetComponent<AudioSource>();
+            audd.Play();
+        }
+        
+    }
+
+}
---
+++ Assets/resizeWithLeftController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+public class resizeWithLeftController : MonoBehaviour
+{
+    public GameObject task;
+
+    public GameObject otherController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            task.transform.localScale += new Vector3(0.1F, 0.1f, 0.1f);
+        } else if (Input.GetKeyDown(KeyCode.S))
+        {
+            task.transform.localScale -= new Vector3(0.1F, 0.1f, 0.1f);
+        }
+        else if (Input.GetKey(KeyCode.C))
+            {
+                task.transform.position = new Vector3(otherController.transform.position.x, otherController.transform.position.y + 0.75f, otherController.transform.position.z);
+                task.transform.rotation = otherController.transform.rotation; 
+            }
+        
+    }
+}
