e70034b7bb371523a9a661e087fbcb13ffd31a1a
Brian Cohn <brian.cohn@usc.edu>
2019-04-28 17:33:18 -0700

    #3 successful saving of files 1s long to desktop

---
+++ Assets/BCTools.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BCTools : MonoBehaviour
+{
+
+    public static string DeltaTimeString(float priorTimeInSeconds)
+    {
+        return (Time.time - priorTimeInSeconds).ToString("F5");
+    }
+ 
+}
--- Assets/RecordTrackedAlias.cs
+++ Assets/RecordTrackedAlias.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
-
+using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 public class RecordTrackedAlias : MonoBehaviour
 {
     public GameObject trackedAlias;
@@ -12,7 +15,7 @@ public class RecordTrackedAlias : MonoBehaviour
     private List<string> trackedObservations = new List<string>();  
     private GameObject controllerL;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         var aliasObject = trackedAlias.transform.GetChild(0);
         hmd = aliasObject.transform.GetChild(1).gameObject;
@@ -21,19 +24,56 @@ public class RecordTrackedAlias : MonoBehaviour
     }
 
     // Update is called once per frame
-
-    void Update()
+    private static string Tracked6DString(GameObject trackedObject)
     {
-        // hmd.transform.position.x, hmd.transform.rotation.eulerAngles;
-
-        Debug.Log(TrackedString(hmd));
-    }
-
-    private string TrackedString(GameObject trackedObject)
-    {
-
+        var position = trackedObject.transform.position;
+        var rotation = trackedObject.transform.rotation;
         var outString =
-            $"{trackedObject.transform.position.x},{trackedObject.transform.position.y},{trackedObject.transform.position.z},{trackedObject.transform.rotation.eulerAngles.x},{trackedObject.transform.rotation.eulerAngles.y},{trackedObject.transform.rotation.eulerAngles.z}";
+            $"{position.x:F4},{position.y:F4},{position.z:F4},{rotation.eulerAngles.x:F4},{rotation.eulerAngles.y:F4},{rotation.eulerAngles.z:F4}";
         return outString;
     }
+
+    private void SavePositionsAndRotationsToDiskAndAnalyze()
+    {
+
+       
+        try
+        {
+            using (var fs = new FileStream(Environment.GetFolderPath(Environment.SpecialFolder.Desktop)+ $"/textme_{Time.frameCount}.csv", FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                using (var fw = new StreamWriter(fs))
+                {
+                    //header
+                    fw.Write("time_ms,hmd_x,hmd_y,hmd_z,hmd_rot_x,hmd_rot_y,hmd_rot_z,lc_x,lc_y,lc_z,lc_rot_x,lc_rot_y,lc_rot_z,rc_x,rc_y,rc_z,rc_rot_x,rc_rot_y,rc_rot_z\n");
+
+                    foreach (var t in trackedObservations)
+                    {
+                        fw.WriteLine(t);
+                        Console.WriteLine(t + "_written");
+                    }
+                    fw.Flush(); // Added
+                }
+            }
+        }
+        catch(IOException)
+        {
+            // ReSharper disable once Unity.PerformanceCriticalCodeInvocation
+            Debug.LogError("FileStream not possible");
+        }
+        
+    }
+
+    private void  Update()
+    {
+        var x = Time.time.ToString("F4" ) + "," + Tracked6DString(hmd) + "," + Tracked6DString(controllerL) +"," + Tracked6DString(controllerR);
+        trackedObservations.Add(x);
+        if (Time.frameCount % 90 == 0)
+        {
+            var startTime = Time.time;
+            SavePositionsAndRotationsToDiskAndAnalyze();
+            trackedObservations.Clear();
+        }
+    }
+
+
 }
