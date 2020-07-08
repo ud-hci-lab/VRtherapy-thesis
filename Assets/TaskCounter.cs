89ea02e2c05d86454f36b4ffd3000fd2b37a9078
laurenbaron <lbaron@udel.edu>
2020-07-08 00:08:15 -0400

    stop fireworks and animation
    after 5 seconds, stop after task victory stuff so can display new menu

--- Assets/TaskCounter.cs
+++ Assets/TaskCounter.cs
@@ -7,9 +7,7 @@ using UnityEngine.SceneManagement;
 public class TaskCounter : MonoBehaviour
 {
     private TaskController taskControllerScript;
-
     private TextMeshProUGUI tmp;
-
     private string totalCount;
     private int totalInt;
     private Animator taskAnimator;
@@ -19,14 +17,20 @@ public class TaskCounter : MonoBehaviour
     //public GameObject victoryChick;
     public GameObject fishDots;
     public GameObject chickDots;
+    int success;
 
     // Start is called before the first frame update
     private void Start()
     {
+        Debug.Log("this: "+ this);
+        Debug.Log("get component " + this.GetComponent<TextMeshProUGUI>());
         tmp = this.GetComponent<TextMeshProUGUI>();
-        tmp.text = "0";
+        tmp.text = "Dots Completed: 0";
         victory= victoryObj.GetComponent<ParticleSystem>();
         victoryFish.SetActive(false);
+        success = 0;
+        timer = 0;
+        timerReached = false;
         //victoryChick.SetActive(false);
     }
 
@@ -56,7 +60,7 @@ public class TaskCounter : MonoBehaviour
             {
                 
                 tmp.text = taskControllerScript.tasksAchieved + totalCount;
-                if (taskControllerScript.tasksAchieved == totalInt && taskAnimator == null)
+                if (taskControllerScript.tasksAchieved == totalInt && taskAnimator == null && success == 0)
                 {
                     Debug.Log("VICTORY");
                     victory.Play(true);
@@ -66,16 +70,24 @@ public class TaskCounter : MonoBehaviour
                     {
                         victoryFish.SetActive(true);
                         fishDots.SetActive(false);
-                    } 
+                    }
                     else
                     {
                         //victoryChick.SetActive(true);
                         chickDots.SetActive(false);
                     }
+
+                    Invoke("stop", 5.0f);
                 }
             }
         }
-       
-        
+    }
+
+    private void stop()
+    {
+        victoryFish.SetActive(false);
+        victory.Stop(true);
+        timerReached = true;
+        success += 1;
     }
 }
