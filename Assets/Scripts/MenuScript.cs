c57632b360032a5afe499cb5ef50698cc041142a
mrdwyer <mdwyer.professional@gmail.com>
2021-12-06 15:34:40 -0500

    Deactivate model on start
    - Deactivate model on start
    - Add pointerclick handler for menuscript to handle error

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -52,6 +52,16 @@ public class MenuScript : MonoBehaviour, IPointerClickHandler
         Debug.Log("<menu script><START> Started!");
     }
 
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        //Use this to tell when the user right-clicks on the Button
+        if (pointerEventData.button == PointerEventData.InputButton.Right)
+        {
+            //Output to console the clicked GameObject's name and the following message. You can replace this with your own actions for when clicking the GameObject.
+            Debug.Log(name + " Game Object Right Clicked!");
+        }
+    }
+    
     public void FishIntialization()
     {
         FishTask.SetActive(true);
